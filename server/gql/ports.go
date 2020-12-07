c6444c3067a8b7d9f14f5f01505b7ff8bfa5dda2
Paolo Grisoli <paolo_g84@icloud.com>
2020-12-07 14:32:37 +0100

    disable mockgen for CI to not fail

--- server/gql/ports.go
+++ server/gql/ports.go
@@ -1,5 +1,5 @@
 package gql
 
-//go:generate mockgen -source ports.go -destination portsmock_test.go -package gql_test
+// ** TODO: fix missing mockgen in CI //go:generate mockgen -source ports.go -destination portsmock_test.go -package gql_test
 
 type App interface{}
